9c30fec354c3fab36d651c8a4ced8e79a730b98a
Eslamadelelkhouly <rodyerak@gmail.com>
2025-08-02 02:03:04 +0300

    add onboading controller notifier

---
+++ lib/src/onboarding/controller/onboarding_notifier.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnboardingNotifier extends ChangeNotifier {
+  int _selectedPage = 0;
+
+  int get selectedPage => _selectedPage;
+  
+  set selectedPage(int page) {
+    _selectedPage = page;
+    notifyListeners();
+  }
+}

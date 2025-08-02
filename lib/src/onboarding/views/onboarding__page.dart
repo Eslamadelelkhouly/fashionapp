f89c44c0d9e8b222ea7c15cea2a9a1f3e7bba954
Eslamadelelkhouly <rodyerak@gmail.com>
2025-08-03 02:35:44 +0300

    finish onboarding screen

--- lib/src/onboarding/views/onboarding__page.dart
+++ lib/src/onboarding/views/onboarding__page.dart
@@ -1,9 +1,13 @@
+import 'package:fashionapp/common/utils/kcolors.dart';
 import 'package:fashionapp/src/onboarding/controller/onboarding_notifier.dart';
 import 'package:fashionapp/src/onboarding/widgets/onboarding__pageone.dart';
 import 'package:fashionapp/src/onboarding/widgets/onboarding__pagetwo.dart';
 import 'package:fashionapp/src/onboarding/widgets/welcom_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:page_view_dot_indicator/page_view_dot_indicator.dart';
 import 'package:provider/provider.dart';
 
 class OnBoardingScreen extends StatefulWidget {
@@ -41,6 +45,77 @@ class _OnBoardingScreenState extends State<OnBoardingScreen> {
               WelcomScreen(),
             ],
           ),
+          context.watch<OnboardingNotifier>().selectedPage == 2
+              ? SizedBox.shrink()
+              : Positioned(
+                  bottom: 50.h,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20.w),
+                    width: ScreenUtil().screenWidth,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        context.watch<OnboardingNotifier>().selectedPage == 0
+                            ? SizedBox(
+                                width: 25,
+                              )
+                            : GestureDetector(
+                                onTap: () {
+                                  _pageController.animateToPage(
+                                    context
+                                            .read<OnboardingNotifier>()
+                                            .selectedPage -
+                                        1,
+                                    duration: Duration(milliseconds: 200),
+                                    curve: Curves.easeIn,
+                                  );
+                                },
+                                child: Icon(
+                                  AntDesign.leftcircleo,
+                                  color: Kolors.kPrimary,
+                                  size: 30,
+                                ),
+                              ),
+                        SizedBox(
+                          width: ScreenUtil().screenWidth * 0.7,
+                          height: 50.h,
+                          child: PageViewDotIndicator(
+                            currentItem: context
+                                .watch<OnboardingNotifier>()
+                                .selectedPage,
+                            count: 3,
+                            unselectedColor: Kolors.kGray,
+                            selectedColor: Kolors.kPrimary,
+                            duration: const Duration(milliseconds: 200),
+                            onItemClicked: (index) {
+                              _pageController.animateToPage(
+                                index,
+                                duration: Duration(milliseconds: 200),
+                                curve: Curves.easeInOut,
+                              );
+                            },
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _pageController.animateToPage(
+                              context.read<OnboardingNotifier>().selectedPage +
+                                  1,
+                              duration: Duration(milliseconds: 200),
+                              curve: Curves.easeIn,
+                            );
+                          },
+                          child: Icon(
+                            AntDesign.rightcircleo,
+                            color: Kolors.kPrimary,
+                            size: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
         ],
       ),
     );
